ff524b7f6e8e60b3820aa43cb9090df1e526c436
newthiesco <newthiesco@gmail.com>
2025-01-26 11:31:12 -0500

    point backend to correct org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "thiesco-Devops"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "terraform-gha-cicd"
     }
   }
 }
