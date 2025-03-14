380bbd9b528f7249fc09c4040b7f0f447049a1c5
ayalnehf <44900744+ayalnehf@users.noreply.github.com>
2025-03-14 11:47:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "Ayalneh-Terraform-Demo"
+    organization = "ACG-Terraform-Demo"
 
     workspaces {
       name = "demo-github-actions"
