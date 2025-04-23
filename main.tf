3b8f2757917a5a103534f03caceffb1d4d3f2762
duythai <118787562+DuyThaiVan@users.noreply.github.com>
2025-04-23 15:53:17 +0700

    commit main"

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "duythai1985-org"
 
     workspaces {
       name = "demo-github-actions"
